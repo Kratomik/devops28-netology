852bbac3070df6249478257765858b2ff3c6e625
Nocolay <lebedkin@signatec.ru>
2023-04-19 08:42:40 +0700

    2python

--- 1.py
+++ 1.py
@@ -2,10 +2,10 @@
 
 import os
 
-bash_command = ["cd ~/Documents/py", "git status"]
+bash_command = ["cd ~/Documents/py/devops28-netology", "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
 
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
         prepare_result = result.replace('\tmodified:   ', '')
-        print(f'/home/sovar/Documents/py/{prepare_result}')
+        print(f'/home/sovar/Documents/py/devops28-netology/{prepare_result}')
